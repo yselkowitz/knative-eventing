4010ea9ba51b3f23ae433be392caa87b6d0a27e4
Matthias Wessendorf <mwessend@redhat.com>
2019-04-25 12:42:16 +0200

    Adding latest release bits

--- openshift/release/resolve.sh
+++ openshift/release/resolve.sh
@@ -20,7 +20,11 @@ function resolve_resources(){
     # 4. Remove comment lines
     # 5. Remove empty lines
     sed -e "s+\(.* image: \)\(github.com\)\(.*/\)\(test/\)\(.*\)+\1\2 \3\4test-\5+g" \
+        -e "s+github.com/knative/eventing/cmd/broker/ingress+${image_prefix}ingress${image_tag}+" \
+        -e "s+github.com/knative/eventing/cmd/broker/filter+${image_prefix}filter${image_tag}+" \
         -e "s+github.com/knative/eventing/pkg/provisioners/inmemory/controller+${image_prefix}in-memory-channel-controller${image_tag}+" \
+        -e "s+github.com/knative/eventing/contrib/kafka/cmd/controller+${image_prefix}kafka-channel-controller${image_tag}+" \
+        -e "s+github.com/knative/eventing/contrib/kafka/cmd/dispatcher+${image_prefix}kafka-channel-dispatcher${image_tag}+" \
         -e "s+\(.* image: \)\(github.com\)\(.*/\)\(.*\)+\1${image_prefix}\4${image_tag}+g" \
         -e '/^[ \t]*#/d' \
         -e '/^[ \t]*$/d' \
