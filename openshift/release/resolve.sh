7e9e0758b86ebf38142af175546d327c2fbc7d26
Matthias Wessendorf <mwessend@redhat.com>
2019-05-08 11:01:28 +0200

    reflect core image changes and updating scripts, yamls to reflect new images

--- openshift/release/resolve.sh
+++ openshift/release/resolve.sh
@@ -15,14 +15,14 @@ function resolve_resources(){
   for yaml in "$dir"/*.yaml; do
     echo "---" >> $resolved_file_name
     # 1. Prefix test image references with test-
-    # 2. Rewrite inmemory controller image reference separately
-    # 3. Rewrite image references
-    # 4. Remove comment lines
-    # 5. Remove empty lines
+    # 2. Rewrite image references
+    # 3. Remove comment lines
+    # 3. Remove empty lines
     sed -e "s+\(.* image: \)\(github.com\)\(.*/\)\(test/\)\(.*\)+\1\2 \3\4test-\5+g" \
         -e "s+github.com/knative/eventing/cmd/broker/ingress+${image_prefix}ingress${image_tag}+" \
         -e "s+github.com/knative/eventing/cmd/broker/filter+${image_prefix}filter${image_tag}+" \
-        -e "s+github.com/knative/eventing/pkg/provisioners/inmemory/controller+${image_prefix}in-memory-channel-controller${image_tag}+" \
+        -e "s+github.com/knative/eventing/cmd/in_memory/controller+${image_prefix}in-memory-channel-controller${image_tag}+" \
+        -e "s+github.com/knative/eventing/cmd/in_memory/dispatcher+${image_prefix}in-memory-channel-dispatcher${image_tag}+" \
         -e "s+github.com/knative/eventing/contrib/kafka/cmd/controller+${image_prefix}kafka-channel-controller${image_tag}+" \
         -e "s+github.com/knative/eventing/contrib/kafka/cmd/dispatcher+${image_prefix}kafka-channel-dispatcher${image_tag}+" \
         -e "s+\(.* image: \)\(github.com\)\(.*/\)\(.*\)+\1${image_prefix}\4${image_tag}+g" \
