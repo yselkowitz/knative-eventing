a0af0c85e24e9b6003ad194083ef64b30890f031
Marek Schmidt <maschmid@redhat.com>
2019-08-01 18:41:18 +0200

    e2e-tests-openshift.sh github.com/knative -> knative.dev update

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
-source $(dirname $0)/../vendor/github.com/knative/test-infra/scripts/e2e-tests.sh
+source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/e2e-tests.sh
 source $(dirname $0)/release/resolve.sh
 
 set -x
