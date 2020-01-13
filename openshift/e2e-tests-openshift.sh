64a14bf0fcfee11197493a352a9d99e74bffca7d
Matthias Wessendorf <mwessend@redhat.com>
2020-01-13 12:57:30 +0100

    Adding back to patching of the operator yaml

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -155,12 +155,11 @@ function install_knative_eventing(){
 
   create_knative_namespace eventing
 
-  # echo ">> Patching Knative Eventing CatalogSource to reference CI produced images"
-  # CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  # RELEASE_YAML="https://raw.githubusercontent.com/matzew/eventing/script_fixes/openshift/release/knative-eventing-ci.yaml"
-  # sed "s|--filename=.*|--filename=${RELEASE_YAML}|"  openshift/olm/knative-eventing.catalogsource.yaml > knative-eventing.catalogsource-ci.yaml
+  echo ">> Patching Knative Eventing CatalogSource to reference CI produced images"
+  CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  RELEASE_YAML="https://raw.githubusercontent.com/openshift/knative-eventing/${CURRENT_GIT_BRANCH}/openshift/release/knative-eventing-ci.yaml"
+  sed "s|--filename=.*|--filename=${RELEASE_YAML}|"  openshift/olm/knative-eventing.catalogsource.yaml > knative-eventing.catalogsource-ci.yaml
 
-  # oc apply -n $OLM_NAMESPACE -f knative-eventing.catalogsource-ci.yaml
   oc apply -n $OLM_NAMESPACE -f openshift/olm/knative-eventing.catalogsource.yaml
   timeout_non_zero 900 '[[ $(oc get pods -n $OLM_NAMESPACE | grep -c knative-eventing) -eq 0 ]]' || return 1
   wait_until_pods_running $OLM_NAMESPACE
