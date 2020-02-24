d88f4726def014e8f9105c820db259c47727c8ff
Matthias Wessendorf <mwessend@redhat.com>
2020-02-24 09:45:27 +0100

    pointing to branch, instead of head (#433)
    * pointing to branch, instead of head
    
    * pointing to branch, instead of head

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -157,7 +157,7 @@ function install_knative_eventing(){
 
   echo ">> Patching Knative Eventing CatalogSource to reference CI produced images"
   CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  RELEASE_YAML="https://raw.githubusercontent.com/openshift/knative-eventing/${CURRENT_GIT_BRANCH}/openshift/release/knative-eventing-ci.yaml,https://raw.githubusercontent.com/openshift-knative/knative-eventing-operator/master/deploy/resources/networkpolicies.yaml"
+  RELEASE_YAML="https://raw.githubusercontent.com/openshift/knative-eventing/${CURRENT_GIT_BRANCH}/openshift/release/knative-eventing-ci.yaml,https://raw.githubusercontent.com/openshift-knative/knative-eventing-operator/v0.12.0/deploy/resources/networkpolicies.yaml"
   sed "s|--filename=.*|--filename=${RELEASE_YAML}|"  openshift/olm/knative-eventing.catalogsource.yaml > knative-eventing.catalogsource-ci.yaml
 
   oc apply -n $OLM_NAMESPACE -f knative-eventing.catalogsource-ci.yaml
