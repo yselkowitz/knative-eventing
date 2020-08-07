cdd2988626cd33812536490ec6446c5cf088d9bd
Matthias Wessendorf <mwessend@redhat.com>
2020-08-07 15:59:28 +0200

    Apply invokefix master (#751)
    * Fixing test invoke
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>
    
    * :lipstick: updates
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>
    
    * :lipstick: updates
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -167,8 +167,10 @@ function run_e2e_tests(){
   local test_name="${1:-}"
   local run_command=""
   local failed=0
-  local channels=messaging.knative.dev/v1beta1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel
-  local common_opts="-channels=$channels --kubeconfig $KUBECONFIG --imagetemplate $TEST_IMAGE_TEMPLATE"
+  local channels=messaging.knative.dev/v1beta1:Channel,messaging.knative.dev/v1beta1:InMemoryChannel,messaging.knative.dev/v1:Channel,messaging.knative.dev/v1:InMemoryChannel
+  local sources=sources.knative.dev/v1alpha2:ApiServerSource,sources.knative.dev/v1alpha2:ContainerSource,sources.knative.dev/v1alpha2:PingSource
+
+  local common_opts=" -channels=$channels -sources=$sources --kubeconfig $KUBECONFIG --imagetemplate $TEST_IMAGE_TEMPLATE"
   if [ -n "$test_name" ]; then
       local run_command="-run ^(${test_name})$"
   fi
@@ -179,7 +181,7 @@ function run_e2e_tests(){
   go_test_e2e -timeout=90m -parallel=12 ./test/e2e \
     "$run_command" \
     -brokerclass=MTChannelBasedBroker \
-    "$common_opts" || failed=$?
+    $common_opts || failed=$?
 
   return $failed
 }
