e2a4ae34ff8dc52bdc8332ac2799659d3bc09b38
Matthias Wessendorf <mwessend@redhat.com>
2020-08-18 16:09:02 +0200

    Fixing the disabled image promotion for all branches (#775)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/ci-operator/update-ci.sh
+++ openshift/ci-operator/update-ci.sh
@@ -19,7 +19,7 @@ test -d "$CONFIGDIR" || fail "'$CONFIGDIR' is not a directory"
 # Generate CI config files
 CONFIG=$CONFIGDIR/openshift-knative-eventing-release-$VERSION
 CURDIR=$(dirname $0)
-$CURDIR/generate-ci-config.sh knative-$VERSION 4.5 true > ${CONFIG}__45.yaml
+$CURDIR/generate-ci-config.sh knative-$VERSION 4.5 > ${CONFIG}__45.yaml
 $CURDIR/generate-ci-config.sh knative-$VERSION 4.6 true > ${CONFIG}__46.yaml
 
 # Switch to openshift/release to generate PROW files
