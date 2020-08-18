a3a720731f71a15d055bed35e83ea6be6fd9841e
Matthias Wessendorf <mwessend@redhat.com>
2020-08-18 09:50:58 +0200

    Adding CRD install label first, to avoid race (#773)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -51,6 +51,7 @@ function install_strimzi(){
   strimzi_version=`curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null`
   header_text "Strimzi install"
   kubectl create namespace kafka
+  kubectl -n kafka apply --selector strimzi.io/crd-install=true -f "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml"
   curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
   | sed 's/namespace: .*/namespace: kafka/' \
   | kubectl -n kafka apply -f -
