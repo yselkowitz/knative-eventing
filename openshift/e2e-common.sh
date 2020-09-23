6edad94cf4a8024321bfd5396fa55118ce6af4ea
Matthias Wessendorf <mwessend@redhat.com>
2020-09-23 15:43:10 +0200

    We run Strimzi/Kafka in contrib (#840)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -50,22 +50,6 @@ function timeout_non_zero() {
   return 0
 }
 
-function install_strimzi(){
-  strimzi_version=`curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null`
-  header_text "Strimzi install"
-  kubectl create namespace kafka
-  kubectl -n kafka apply --selector strimzi.io/crd-install=true -f "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml"
-  curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
-  | sed 's/namespace: .*/namespace: kafka/' \
-  | kubectl -n kafka apply -f -
-
-  header_text "Applying Strimzi Cluster file"
-  kubectl -n kafka apply -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
-
-  header_text "Waiting for Strimzi to become ready"
-  sleep 5; while echo && kubectl get pods -n kafka | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
-}
-
 function install_serverless(){
   header "Installing Serverless Operator"
   local operator_dir=/tmp/serverless-operator
