306f45147aecb464029edb0127e4b8a6319af9ba
Matthias Wessendorf <mwessend@redhat.com>
2020-10-22 18:11:31 +0200

    Removing 4.5 channel, we are defaulting to 4.6 (#900)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/ci-operator/update-ci.sh
+++ openshift/ci-operator/update-ci.sh
@@ -19,8 +19,7 @@ test -d "$CONFIGDIR" || fail "'$CONFIGDIR' is not a directory"
 # Generate CI config files
 CONFIG=$CONFIGDIR/openshift-knative-eventing-release-$VERSION
 CURDIR=$(dirname $0)
-$CURDIR/generate-ci-config.sh knative-$VERSION 4.5 > ${CONFIG}__45.yaml
-$CURDIR/generate-ci-config.sh knative-$VERSION 4.6 true > ${CONFIG}__46.yaml
+$CURDIR/generate-ci-config.sh knative-$VERSION 4.6 > ${CONFIG}__46.yaml
 
 # Switch to openshift/release to generate PROW files
 cd $OPENSHIFT
