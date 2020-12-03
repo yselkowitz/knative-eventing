b384dd665ecb5d5d29d7102c10ea00e78151da22
Matthias Wessendorf <mwessend@redhat.com>
2020-12-03 08:19:48 +0100

    Adding OCP 4.7 run to CI job (#1012)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/ci-operator/update-ci.sh
+++ openshift/ci-operator/update-ci.sh
@@ -20,6 +20,7 @@ test -d "$CONFIGDIR" || fail "'$CONFIGDIR' is not a directory"
 CONFIG=$CONFIGDIR/openshift-knative-eventing-release-$VERSION
 CURDIR=$(dirname $0)
 $CURDIR/generate-ci-config.sh knative-$VERSION 4.6 > ${CONFIG}__46.yaml
+$CURDIR/generate-ci-config.sh knative-$VERSION 4.7 true > ${CONFIG}__47.yaml
 
 # Switch to openshift/release to generate PROW files
 cd $OPENSHIFT
