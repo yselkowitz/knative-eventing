a64d7147eebbc7e12a69a348fb093f2e38e5d303
Matthias Wessendorf <mwessend@redhat.com>
2021-05-10 13:19:54 +0200

    Adding 4.8 jobs to CI config generator (#1264)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/ci-operator/update-ci.sh
+++ openshift/ci-operator/update-ci.sh
@@ -35,6 +35,7 @@ CONFIG=$CONFIGDIR/openshift-knative-eventing-release-$VERSION
 PERIODIC_CONFIG=$PERIODIC_CONFIGDIR/openshift-knative-eventing-release-$VERSION-periodics.yaml
 CURDIR=$(dirname $0)
 $CURDIR/generate-ci-config.sh knative-$VERSION 4.7 > ${CONFIG}__47.yaml
+$CURDIR/generate-ci-config.sh knative-$VERSION 4.8 true > ${CONFIG}__48.yaml
 
 # Append missing lines to the mirror file.
 VER=$(echo $VERSION | sed 's/\./_/;s/\.[0-9]\+$//') # X_Y form of version
