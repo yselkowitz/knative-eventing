f928769dc843ce8c36e64c1204b2c2c5d45629d6
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2021-11-19 10:22:14 -0800

    don't generate periodics for nightly and fix pomotion name (#1489)
    Signed-off-by: Ahmed Abdalla <aabdelre@redhat.com>
    
    Co-authored-by: Ahmed Abdalla <aabdelre@redhat.com>

--- openshift/ci-operator/update-ci.sh
+++ openshift/ci-operator/update-ci.sh
@@ -39,36 +39,50 @@ $CURDIR/generate-ci-config.sh knative-$VERSION 4.8 true > ${CONFIG}__48.yaml
 $CURDIR/generate-ci-config.sh knative-$VERSION 4.9 true > ${CONFIG}__49.yaml
 
 # Append missing lines to the mirror file.
-VER=$(echo $VERSION | sed 's/\./_/;s/\.[0-9]\+$//') # X_Y form of version
-MIRROR="$OPENSHIFT/core-services/image-mirroring/knative/mapping_knative_${VER}_quay"
-[ -n "$(tail -c1 $MIRROR)" ] && echo >> $MIRROR # Make sure there's a newline
-test_images=$(find ./openshift/ci-operator/knative-test-images -mindepth 1 -maxdepth 1 -type d | LC_COLLATE=posix sort)
-for IMAGE in $test_images; do
-    NAME=knative-eventing-test-$(basename $IMAGE | sed 's/_/-/' | sed 's/_/-/' | sed 's/[_.]/-/' | sed 's/[_.]/-/' | sed 's/v0/upgrade-v0/')
+if [[ "$VERSION" != "next" ]]; then
+  VER=$(echo $VERSION | sed 's/\./_/;s/\.[0-9]\+$//') # X_Y form of version
+  MIRROR="$OPENSHIFT/core-services/image-mirroring/knative/mapping_knative_${VER}_quay"
+  [ -n "$(tail -c1 $MIRROR)" ] && echo >> $MIRROR # Make sure there's a newline
+  test_images=$(find ./openshift/ci-operator/knative-test-images -mindepth 1 -maxdepth 1 -type d | LC_COLLATE=posix sort)
+  for IMAGE in $test_images; do
+      NAME=knative-eventing-test-$(basename $IMAGE | sed 's/_/-/' | sed 's/_/-/' | sed 's/[_.]/-/' | sed 's/[_.]/-/' | sed 's/v0/upgrade-v0/')
 
-    echo "Adding $NAME to mirror file as $VERSION tag"
-    LINE="registry.ci.openshift.org/openshift/knative-$VERSION.0:$NAME quay.io/openshift-knative/${NAME/knative-eventing-test-/}:$VERSION"
-    # Add $LINE if not already present
-    grep -q "^$LINE\$" $MIRROR || echo "$LINE"  >> $MIRROR
+      echo "Adding $NAME to mirror file as $VERSION tag"
+      LINE="registry.ci.openshift.org/openshift/knative-$VERSION.0:$NAME quay.io/openshift-knative/${NAME/knative-eventing-test-/}:$VERSION"
+      # Add $LINE if not already present
+      grep -q "^$LINE\$" $MIRROR || echo "$LINE"  >> $MIRROR
 
-    VER=$(echo $VER | sed 's/\_/./')
-    echo "Adding $NAME to mirror file as $VER tag"
-    LINE="registry.ci.openshift.org/openshift/knative-$VERSION.0:$NAME quay.io/openshift-knative/${NAME/knative-eventing-test-/}:$VER"
-    # Add $LINE if not already present
-    grep -q "^$LINE\$" $MIRROR || echo "$LINE"  >> $MIRROR
-done
-
+      VER=$(echo $VER | sed 's/\_/./')
+      echo "Adding $NAME to mirror file as $VER tag"
+      LINE="registry.ci.openshift.org/openshift/knative-$VERSION.0:$NAME quay.io/openshift-knative/${NAME/knative-eventing-test-/}:$VER"
+      # Add $LINE if not already present
+      grep -q "^$LINE\$" $MIRROR || echo "$LINE"  >> $MIRROR
+  done
+else
+  MIRROR="$OPENSHIFT/core-services/image-mirroring/knative/mapping_knative_nightly_quay"
+  [ -n "$(tail -c1 $MIRROR)" ] && echo >> $MIRROR # Make sure there's a newline
+  test_images=$(find ./openshift/ci-operator/knative-test-images -mindepth 1 -maxdepth 1 -type d | LC_COLLATE=posix sort)
+  for IMAGE in $test_images; do
+      NAME=knative-eventing-test-$(basename $IMAGE | sed 's/_/-/' | sed 's/_/-/' | sed 's/[_.]/-/' | sed 's/[_.]/-/' | sed 's/v0/upgrade-v0/')
+      echo "Adding $NAME to mirror file as latest tag"
+      LINE="registry.ci.openshift.org/openshift/knative-nightly:$NAME quay.io/openshift-knative/${NAME/knative-eventing-test-/}:latest"
+      # Add $LINE if not already present
+      grep -q "^$LINE\$" $MIRROR || echo "$LINE"  >> $MIRROR
+  done
+fi
 # Switch to openshift/release to generate PROW files
 cd $OPENSHIFT
 echo "Generating PROW files in $OPENSHIFT"
 make jobs
 make ci-operator-config
 # We have to do this manually, see: https://docs.ci.openshift.org/docs/how-tos/notification/
-echo "==== Adding reporter_config to periodics ===="
-# These version MUST match the ocp version we used above
-for OCP_VERSION in 47; do
-    sed -i "/  name: periodic-ci-openshift-knative-eventing-release-${VERSION}-${OCP_VERSION}-e2e-aws-ocp-${OCP_VERSION}-continuous\n  spec:/ r $TMPDIR/reporterConfig" "$PERIODIC_CONFIG"
-done
+if [[ "$VERSION" != "next" ]]; then
+  echo "==== Adding reporter_config to periodics ===="
+  # These version MUST match the ocp version we used above
+  for OCP_VERSION in 47 48; do
+      sed -i "/  name: periodic-ci-openshift-knative-eventing-release-${VERSION}-${OCP_VERSION}-e2e-aws-ocp-${OCP_VERSION}-continuous\n  spec:/ r $TMPDIR/reporterConfig" "$PERIODIC_CONFIG"
+  done
+fi
 echo "==== Changes made to $OPENSHIFT ===="
 git status
 echo "==== Commit changes to $OPENSHIFT and create a PR"
