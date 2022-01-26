7ac8401ac6fb49686852287cf9da2c93cf49b491
Serverless QE Robot <ocf-qe@redhat.com>
2022-01-25 19:35:33 -0500

    Add openshift specific files.

---
+++ openshift/e2e-tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1090
+source "$(dirname "$0")/../vendor/knative.dev/hack/e2e-tests.sh"
+source "$(dirname "$0")/e2e-common.sh"
+
+set -Eeuox pipefail
+
+export TEST_IMAGE_TEMPLATE="${EVENTING_TEST_IMAGE_TEMPLATE}"
+
+env
+
+failed=0
+
+(( !failed )) && install_serverless || failed=1
+
+(( !failed )) && install_knative_eventing || failed=1
+
+(( !failed )) && install_tracing || failed=1
+
+(( !failed )) && run_e2e_tests || failed=1
+
+(( !failed )) && uninstall_knative_eventing || failed=1
+
+(( failed )) && dump_cluster_state
+
+(( failed )) && exit 1
+
+success
